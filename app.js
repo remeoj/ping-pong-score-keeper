c68053a1106a3941e5bd9104e3def5bd304468ad
JoemerRamos <34356209+JoemerRamos@users.noreply.github.com>
2021-05-07 23:09:45 -0400

    Update: Small changes to optimize functions

--- app.js
+++ app.js
@@ -52,12 +52,10 @@ scoreLimit.addEventListener("change", function () {
 
 function reset() {
   winningStatus = false;
-  p1.score = 0;
-  p2.score = 0;
-  p1.display.textContent = 0;
-  p1.display.classList.remove("has-text-success", "has-text-danger");
-  p2.display.textContent = 0;
-  p2.display.classList.remove("has-text-success", "has-text-danger");
-  p1.button.disabled = false;
-  p2.button.disabled = false;
+  for (let p of [p1, p2]) {
+    p.score = 0;
+    p.display.textContent = 0;
+    p.display.classList.remove("has-text-success", "has-text-danger");
+    p.button.disabled = false;
+  }
 }
